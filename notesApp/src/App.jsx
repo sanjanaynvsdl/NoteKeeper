924e0ae5b9c1c8110b544abaeea62099655f0823
sanjanaynvsdl <sanjanayalamarthi@gmail.com>
2024-10-27 20:56:52 +0530

    Added default-path

--- notesApp/src/App.jsx
+++ notesApp/src/App.jsx
@@ -3,14 +3,21 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import SignUp from './pages/SignUp/SignUp'
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom'
+
+const ProtectedRoute = ({ element }) => {
+  const isAuthenticated = localStorage.getItem('token'); // Check if user is logged in
+  return isAuthenticated ? element : <Navigate to="/login" replace />;
+};
 
 const routes=(
   <Router>
     <Routes>
-      <Route path="/dashboard" exact element={<Home/>}/>
+      <Route path="/dashboard" exact element={<ProtectedRoute element={<Home />} />}/>
       <Route path="/login" exact element={<Login/>}/>
       <Route path="/signUp" exact element={<SignUp/>}/>
+      {/* added default path */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   </Router>
 )
